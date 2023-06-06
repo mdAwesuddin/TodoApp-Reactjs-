f45e1375eac2877a83f20a91dec5a3e88f3eb9a9
Awesuddin Mohammed <86012930+mdAwesuddin@users.noreply.github.com>
2023-06-06 13:39:00 +0530

    Add files via upload

---
+++ TodoApp/src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        <p>This App is Made using ReactJs. With this app you an add your daily Todo's to keep up your schedule...</p>
+        <a href=''>Code</a>
+    </div>
+  )
+
+}
+
